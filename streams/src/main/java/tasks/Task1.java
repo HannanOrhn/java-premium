8ec1dfd0730b55725c731aced40d36e7805ac806
Bilal Orhan <hannanorhn00@gmail.com>
2024-02-27 13:57:46 -0800

    streams part1 is completed

---
+++ streams/src/main/java/tasks/Task1.java
@@ -0,0 +1,16 @@
+package task1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1 {
+    //Given a list of words, print the number of characters for each word.
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("hope","honest","happiness","sunshine");
+        findChar(words);
+    }
+    public static void findChar(List<String> list){
+        list.stream()
+                .forEach(p-> System.out.println(p.length()));
+    }
+}
